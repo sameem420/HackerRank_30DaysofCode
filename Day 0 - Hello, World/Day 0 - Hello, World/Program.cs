3ab5bc7f34ade9c7ebf749ab0eaf886e91f131b7
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2020-03-17 00:49:10 +0500

    Day 0 - Hello World Challenge
    Take input from the user and display it with Hello, World on the console.

---
+++ Day 0 - Hello, World/Day 0 - Hello, World/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_0___Hello__World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare a variable named 'inputString' to hold our input.
+            String inputString;
+
+            // Read a full line of input from stdin (cin) and save it to our variable, input_string.
+            inputString = Console.ReadLine();
+
+            // Print a string literal saying "Hello, World." to stdout using cout.
+            Console.WriteLine("Hello, World.");
+
+            // TODO: Write a line of code here that prints the contents of input_string to stdout.
+            Console.WriteLine("{0}", inputString);
+        }
+    }
+}
