9a3789450aa4e1c37f2a7ff6ea624ebfecabd10a
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2020-03-17 19:05:22 +0500

    Day 1 - Data Types Challenge
    Intilization and use of different data types in C-Sharp (C#)

---
+++ Day 1 - Data Types/Day 1 - Data Types/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_1___Data_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 4;
+            double d = 4.0;
+            string s = "Hacker Rank ";
+            // Declare second integer, double, and String variables.
+            int num;
+            double Number;
+            string myString;
+            // Read and save an integer, double, and String to your variables.
+            num = Convert.ToInt32(Console.ReadLine());
+            Number = Convert.ToDouble(Console.ReadLine());
+            myString = Console.ReadLine();
+            // Print the sum of both integer variables on a new line.
+            Console.WriteLine("{0}", num + i);
+            // Print the sum of the double variables on a new line.
+            Console.WriteLine("{0:F1}", Number + d);
+            // Concatenate and print the String variables on a new line
+            // The 's' variable above should be printed first.
+            Console.WriteLine("{0}", s + myString);
+        }
+    }
+}
