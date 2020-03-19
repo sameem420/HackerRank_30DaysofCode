73fc2aeb5833157ae8e5a246823d510316356e31
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2020-03-19 16:54:40 +0500

    Day 3: Intro to Conditional Statements
    Program to use the Conditional statements in c-sharp (C#)

---
+++ Day 3 - Intro to Conditional Statements/Day 3 - Intro to Conditional Statements/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_3___Intro_to_Conditional_Statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            if (N % 2 != 0)
+            {
+                Console.WriteLine("Weird");
+            }
+            else if (N % 2 == 0)
+            {
+                if (N > 2 && N <= 5)
+                {
+                    Console.WriteLine("Not Weird");
+                }
+                else if (N > 5 && N <= 20)
+                {
+                    Console.WriteLine("Weird");
+                }
+                else if (N > 20)
+                {
+                    Console.WriteLine("Not Weird");
+                }
+            }
+        }
+    }
+}
