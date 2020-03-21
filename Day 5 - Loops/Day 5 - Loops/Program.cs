4c1682897f3609562f574fec2809e23198228061
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2020-03-21 21:57:06 +0500

    Day 5 - Loops
    Printing table of any number using the loop's in c-sharp (C#)

---
+++ Day 5 - Loops/Day 5 - Loops/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_5___Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("{0}{1}{2}{3}{4}", n, " x ", i, " = ", n * i);
+            }
+        }
+    }
+}
