912df18bada6fc4e9481ffbf7d7a228ac7f97577
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2020-03-22 19:06:32 +0500

    Create Program.cs

---
+++ Day 6 - Lets Review/Day 6 - Lets Review/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_6___Lets_Review
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+
+            int N = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < N; i++)
+            {
+                string S = Console.ReadLine();
+                for (int j = 0; j < S.Length; j++)
+                {
+                    if (j % 2 == 0)
+                        Console.Write(S[j]);
+                }
+                Console.Write(" ");
+                for (var j = 0; j < S.Length; j++)
+                {
+                    if (j % 2 != 0)
+                        Console.Write(S[j]);
+                }
+
+                Console.Write(Environment.NewLine);
+            }
+        }
+    }
+}
