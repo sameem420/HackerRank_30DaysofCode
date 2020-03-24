ed3f648affaa6a1c14b01d7684c694fab48691da
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2020-03-24 21:30:50 +0500

    Day 7 - Arrays
    Implementation of Array with foreach loop to reverse the Array in c-sharp(C#)

---
+++ Day 7 - Arrays/Day 7 - Arrays/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_7___Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+            Array.Reverse(arr);
+            foreach (int i in arr)
+            {
+                Console.Write(i + " ");
+            }
+            Console.Read();
+        }
+    }
+}
