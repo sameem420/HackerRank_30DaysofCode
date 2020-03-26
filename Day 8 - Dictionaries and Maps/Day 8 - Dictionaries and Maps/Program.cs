b416d5a281bcf4fbfa8fbc8baf2d8e05d327d651
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2020-03-26 17:04:37 +0500

    Create Program.cs

---
+++ Day 8 - Dictionaries and Maps/Day 8 - Dictionaries and Maps/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_8___Dictionaries_and_Maps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            Dictionary<string, int> phoneBook = new Dictionary<string, int>(N);
+            for (int i = 0; i < N; i++)
+            {
+                string[] temp = Console.ReadLine().Split(' ');
+                if (temp[1].Length == 8)
+                {
+                    phoneBook.Add(temp[0], Convert.ToInt32(temp[1]));
+                }
+            }
+            string nameToSearch = "";
+            while ((nameToSearch = Console.ReadLine()) != null)
+            {
+                int flagFound = 0;
+                if (nameToSearch != "")
+                {
+                    if (phoneBook.ContainsKey(nameToSearch))
+                    {
+                        flagFound = 1;
+                    }
+                }
+                if (flagFound == 1)
+                {
+                    Console.WriteLine(nameToSearch + "=" + phoneBook[nameToSearch]);
+                }
+                else
+                {
+                    Console.WriteLine("Not found");
+                }
+            }
+        }
+    }
+}
